fb846261e847da8b00ac09973451f89480707419
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2023-11-12 00:44:43 -0300

    chore(themes): changing from .theme file to .sh

---
+++ src/everforest.sh
@@ -0,0 +1,4 @@
+theme_neutral_100="#9DA9A0"
+theme_neutral_200="#3D484D"
+theme_neutral_400="#2E383C"
+theme_primary="#A7C080"
---
+++ src/nord.sh
@@ -0,0 +1,4 @@
+theme_neutral_100="#E5E9F0"
+theme_neutral_200="#4C566A"
+theme_neutral_400="#3C4251"
+theme_primary="#94BECE"
